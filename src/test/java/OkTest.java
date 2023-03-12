c385a9cb42f3154bd82d13cd820fbd064af31bea
Vladislav Pochernin <vspochernin@gmail.com>
2023-03-12 17:10:54 +0300

    hw1: extract constants

--- src/test/java/OkTest.java
+++ src/test/java/OkTest.java
@@ -9,6 +9,14 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class OkTest {
 
+    public static final String OK_RU = "https://ok.ru/";
+    public static final String LOGIN_ENV = "LOGIN";
+    public static final String PASS_ENV = "PASS";
+    public static final String CORRECT_NAME = "Владислав Почернин";
+    public static final String INCORRECT_LOGIN = "+1234567890";
+    public static final String INCORRECT_PASS = "qwerty";
+    public static final String ERROR_SIGN = "Неправильно указан логин и/или пароль";
+
     @BeforeAll
     public static void beforeAll() {
         Configuration.browser = "chrome";
@@ -21,17 +29,17 @@ public class OkTest {
 
     @Test
     public void correctLogin() {
-        open("https://ok.ru/", LoginPage.class)
-                .login(System.getenv("LOGIN"), System.getenv("PASS"))
+        open(OK_RU, LoginPage.class)
+                .login(System.getenv(LOGIN_ENV), System.getenv(PASS_ENV))
                 .getNameSign()
-                .shouldHave(text("Владислав Почернин"));
+                .shouldHave(text(CORRECT_NAME));
     }
 
     @Test
     public void incorrectLogin() {
-        open("https://ok.ru/", LoginPage.class)
-                .login("+1234567890", "qwerty")
+        open(OK_RU, LoginPage.class)
+                .login(INCORRECT_LOGIN, INCORRECT_PASS)
                 .getFailedLoginSign()
-                .shouldHave(text("Неправильно указан логин и/или пароль"));
+                .shouldHave(text(ERROR_SIGN));
     }
 }
